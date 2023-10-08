cb92b28b33a65f3f41d5e4ad3e99c319978b136b
Taise M <56640569+tmiyazumi@users.noreply.github.com>
2023-10-08 18:05:05 -0400

    local storage

--- client/src/Components/HomePage.js
+++ client/src/Components/HomePage.js
@@ -19,6 +19,7 @@ const HomePage = () => {
     const handleSignOut = () => {
         // Clear the sign-in state in localStorage
         localStorage.removeItem('isSignedIn');
+        localStorage.removeItem('userEmail');
         // Set isSignedIn state to false
         setIsSignedIn(false);
       };
