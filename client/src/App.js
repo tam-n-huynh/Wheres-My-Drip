0f28fa697a2c5c4916575efe288d77e51dae1764
Tam Huynh <tamhuynh3861@gmail.com>
2023-10-08 12:34:46 -0400

    Added routeing

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,20 @@
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import React, {useEffect, useState} from 'react';
+
+//Pages
+import HomePage from './Components/HomePage';
+import MapsPage from './Components/MapsPage';
 
 
 function App() {
   return (
-    <div>
-      Hello World
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePage/>}/>
+          <Route path="/maps" element={<MapsPage/>}/>
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ client/src/Components/MapsPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MapsPage = () => {
+    return (
+        <div>
+            Maps Page
+        </div>
+    )
+}
+
+export default MapsPage;
