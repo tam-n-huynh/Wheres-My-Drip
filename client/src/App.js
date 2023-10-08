d43f1af54a3ac14ca32fab00e8e701a323028829
Tam Huynh <tamhuynh3861@gmail.com>
2023-10-08 16:50:59 -0400

    Maps Changes

--- client/src/App.js
+++ client/src/App.js
@@ -7,12 +7,15 @@ import MapsPage from './Components/MapsPage';
 
 
 function App() {
+  const defaultCenter = { lat: 32, lng: -82 }; // Replace with your desired coordinates
+  const defaultZoom = 10;
+
   return (
     <div className="App">
       <Router>
         <Routes>
           <Route path="/" element={<HomePage/>}/>
-          <Route path="/maps" element={<MapsPage/>}/>
+          <Route path="/maps" element={<MapsPage center={defaultCenter} zoom={defaultZoom}/>}/>
         </Routes>
       </Router>
     </div>
